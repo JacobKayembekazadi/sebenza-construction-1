3ba66acb46bde55ab70a97cf40ba98ebc88b681e
JACOB KAYEMBE KAZADI <jacobkayembekazadi@gmail.com>
2025-07-03 17:25:29 +0000

    its hard to navigate when im editing invoice

--- src/components/add-edit-invoice-dialog.tsx
+++ src/components/add-edit-invoice-dialog.tsx
@@ -142,7 +142,7 @@ export function AddEditInvoiceDialog({
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="sm:max-w-md">
+      <DialogContent className="sm:max-w-md max-h-[90vh] overflow-y-auto">
         <DialogHeader>
           <DialogTitle>{invoice ? "Edit Invoice" : "Add New Invoice"}</DialogTitle>
           <DialogDescription>
