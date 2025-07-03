71855728a755b1f3c6d86ef195924f3d1ce5772e
JACOB KAYEMBE KAZADI <jacobkayembekazadi@gmail.com>
2025-07-03 16:40:12 +0000

    can you fix the navigation, im not able to view the full window when i c

--- src/components/add-edit-estimate-dialog.tsx
+++ src/components/add-edit-estimate-dialog.tsx
@@ -145,7 +145,7 @@ export function AddEditEstimateDialog({
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="max-w-4xl">
+      <DialogContent className="max-w-4xl max-h-[90vh] overflow-y-auto">
         <DialogHeader>
           <DialogTitle>{estimate ? "Edit Estimate" : "Create New Estimate"}</DialogTitle>
           <DialogDescription>
